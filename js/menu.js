7e4e05927cb67d1343561711622c420a22b3bba0
Rick <46992687+rick-77-2006@users.noreply.github.com>
2022-07-25 19:36:49 +0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector(".mobile__button"),
+    closeMenuBtn: document.querySelector(".menu-btn__close"),
+    menu: document.querySelector(".mobile-menu"),
+    body: document.querySelector("body"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
